e8347e0b674e57e63b4393832b2b5a70bc548740
jzeferino <jorgevalentzeferino@gmail.com>
2018-10-18 17:39:04 +0100

    Made all indicators visible with autolayout.

--- src/Xamarin.iOS.DGActivityIndicatorViewBinding.Sample/ViewController.cs
+++ src/Xamarin.iOS.DGActivityIndicatorViewBinding.Sample/ViewController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using CoreGraphics;
 using UIKit;
@@ -17,35 +17,50 @@ namespace Xamarin.iOS.DGActivityIndicatorViewBinding.Sample
 
             View.BackgroundColor = UIColor.FromRGB(237, 85, 101);
 
+            // Thats a 🔨 way of getting safearea insets.
+            var window = UIApplication.SharedApplication.Windows?.FirstOrDefault();
+            var topPadding = window?.SafeAreaInsets.Top ?? 0;
+            var bottomPadding = window?.SafeAreaInsets.Bottom ?? 0;
+
             var cols = 4;
-            var rows = 8;
-            var cellWidth = (int)(View.Bounds.Size.Width / cols);
-            var cellHeight = (int)(View.Bounds.Size.Height / rows);
+            var rows = 9;
+            var cellWidth = View.Bounds.Size.Width / cols;
+            var cellHeight = (View.Bounds.Size.Height - (topPadding + bottomPadding)) / rows;
 
             var enumValues = Enum.GetValues(typeof(DGActivityIndicatorAnimationType)).Cast<DGActivityIndicatorAnimationType>();
 
             for (int i = 0; i < enumValues.Count(); i++)
             {
-                int x = i % cols * cellWidth;
-                int y = i / cols * cellHeight;
+                var x = i % cols * cellWidth;
+                var y = (i / cols * cellHeight) + topPadding;
 
-                var activityIndicatorView = new DGActivityIndicatorView(enumValues.ElementAt(i), UIColor.White);
-
-                activityIndicatorView.Frame = new CGRect(x, y, cellWidth, cellHeight);
-                activityIndicatorView.Bounds = activityIndicatorView.Frame;
-
-                var label = new UILabel(new CGRect(x, y + (cellHeight - activityIndicatorView.Frame.Size.Height), cellWidth, cellHeight));
-                label.Text = i.ToString();
-                label.TextColor = UIColor.White;
-                label.Font = label.Font.WithSize(12f);
+                var activityIndicatorView = new DGActivityIndicatorView(enumValues.ElementAt(i), UIColor.White)
+                {
+                    TranslatesAutoresizingMaskIntoConstraints = false
+                };
+                var label = new UILabel
+                {
+                    TranslatesAutoresizingMaskIntoConstraints = false,
+                    Text = enumValues.ElementAt(i).ToString(),
+                    TextColor = UIColor.White,
+                    TextAlignment = UITextAlignment.Center
+                };
+                label.Font = label.Font.WithSize(9f);
 
                 View.AddSubview(activityIndicatorView);
-                View.AddSubview(label);
+                activityIndicatorView.AddSubview(label);
+
+                activityIndicatorView.HeightAnchor.ConstraintEqualTo(cellHeight).Active = true;
+                activityIndicatorView.WidthAnchor.ConstraintEqualTo(cellWidth).Active = true;
+                activityIndicatorView.LeadingAnchor.ConstraintEqualTo(View.LeadingAnchor, x).Active = true;
+                activityIndicatorView.TopAnchor.ConstraintEqualTo(View.TopAnchor, y).Active = true;
+
+                label.LeadingAnchor.ConstraintEqualTo(activityIndicatorView.LeadingAnchor).Active = true;
+                label.TrailingAnchor.ConstraintEqualTo(activityIndicatorView.TrailingAnchor).Active = true;
+                label.TopAnchor.ConstraintEqualTo(activityIndicatorView.TopAnchor).Active = true;
 
                 activityIndicatorView.StartAnimating();
             }
-
         }
-
     }
 }
